b2f6e588a31d965d8283bfd58001902bf78df259
Elroy Kanye <elroykanye@gmail.com>
2022-01-19 07:38:48 +0100

    initial commit

---
+++ App1Greetings/App1Greetings/App1Greetings/GreetingsPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1Greetings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+
+        {
+            this.Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms!",
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                VerticalOptions = LayoutOptions.Center
+            };
+            this.Content = new Label
+            {
+                Text = "My name is Elroy Kanye, Software Developer",
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+            };
+
+            switch(Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    this.Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                default:
+                    this.Padding = new Thickness(0,0, 0, 0);
+                    break;
+            }
+
+
+            this.Padding = new Thickness(0, 20, 0, 20);
+
+        }
+    }
+}
---
+++ App1SayHello/App1SayHello/App1SayHello/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1SayHello
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            /*
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            */
+
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms"
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
