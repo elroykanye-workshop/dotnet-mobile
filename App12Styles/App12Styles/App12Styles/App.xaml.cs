5f0b6e3a6da8cfa0aa1f8515983a3425630b362c
Elroy Kanye <elroykanye@gmail.com>
2022-03-14 23:27:58 +0100

    completed styles

---
+++ App12Styles/App12Styles/App12Styles/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using App12Styles.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App12Styles
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ColoredButtonsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App12Styles/App12Styles/App12Styles/Pages/ColoredButtonsPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App12Styles.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ColoredButtonsPage : ContentPage
+    {
+        Style defaultButtonStyle;
+        public ColoredButtonsPage()
+        {
+            InitializeComponent();
+            defaultButtonStyle = (Style) Resources["btnStyle"];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Resources["btnStyle"] = defaultButtonStyle;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Resources["btnStyle"] = Resources["btnStyle1"];
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Resources["btnStyle"] = Resources["btnStyle2"];
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            Resources["btnStyle"] = Resources["btnStyle3"];
+        }
+    }
+}
