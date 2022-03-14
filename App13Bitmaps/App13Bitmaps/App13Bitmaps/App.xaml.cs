67e4a812609f56bb277029a849a122b6f9a28e7f
Elroy Kanye <elroykanye@gmail.com>
2022-03-14 23:29:34 +0100

    start bitmaps

---
+++ App13Bitmaps/App13Bitmaps/App13Bitmaps/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using App13Bitmaps.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App13Bitmaps
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GithubImagePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App13Bitmaps/App13Bitmaps/App13Bitmaps/Pages/GithubImagePage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App13Bitmaps.Pages
+{
+    public class GithubImagePage : ContentPage
+    {
+        public GithubImagePage()
+        {
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    new Image
+                    {
+                        Source = ImageSource.FromUri(new Uri("https://avatars.githubusercontent.com/u/77556066?v=4")),
+
+                    },
+                    new Label
+                    {
+                        Text = "Elroy Vince!",
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        FontAttributes = FontAttributes.Bold,
+                    }
+                }
+            };
+        }
+    }
+}
