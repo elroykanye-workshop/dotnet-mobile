c546712809b771f12039df65be1f674a198eb145
Elroy Kanye <elroykanye@gmail.com>
2022-03-24 15:01:45 +0100

    complete abs

--- App14AbsoluteLayout/App14AbsoluteLayout/App14AbsoluteLayout/App.xaml.cs
+++ App14AbsoluteLayout/App14AbsoluteLayout/App14AbsoluteLayout/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App14AbsoluteLayout.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace App14AbsoluteLayout
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AbsoluteDemo();
         }
 
         protected override void OnStart()
---
+++ App14AbsoluteLayout/App14AbsoluteLayout/App14AbsoluteLayout/Pages/AbsoluteDemo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App14AbsoluteLayout.Pages
+{
+    public class AbsoluteDemo : ContentPage
+    {
+        public AbsoluteDemo()
+        {
+
+            AbsoluteLayout absoluteLayout = new AbsoluteLayout
+            {
+                Padding = new Thickness(48),
+            };
+            absoluteLayout.Children.Add(new BoxView
+            {
+                Color = Color.Accent,
+            }, new Rectangle(0, 10, 200, 5));
+            absoluteLayout.Children.Add(new BoxView
+            {
+                Color = Color.Accent,
+            }, new Rectangle(0, 10, 200, 5));
+            absoluteLayout.Children.Add(new BoxView
+            {
+                Color = Color.Accent,
+            }, new Rectangle(10, 0, 5, 65));
+            absoluteLayout.Children.Add(new BoxView
+            {
+                Color = Color.Accent,
+            }, new Rectangle(10, 0, 5, 65));
+
+            absoluteLayout.Children.Add(new Label
+            {
+                Text = "Stylish Header",
+                FontSize = 24,
+            }, new Point(30, 25));
+            absoluteLayout.Children.Add(new Label
+            {
+                FormattedText = new FormattedString
+                {
+                    Spans =
+                    {
+                        new Span
+                        {
+                            Text = "Although the ",
+                        },
+                        new Span
+                        {
+                            Text = "AbsoluteLayout",
+                            FontAttributes = FontAttributes.Italic,
+                        },
+                        new Span
+                        {
+                            Text = " is usually employed for purposes other" +
+                            " than the dislay of text using "
+                        }, new Span
+                        {
+                            Text = "Label",
+                            FontAttributes = FontAttributes.Italic,
+                        },
+                        new Span
+                        {
+                            Text = ", obviously it can be used in that way. " +
+                            "The text continues to wrap nicely " +
+                            "within the bounds of the container" +
+                            " and any padding that may be applied."
+                        }
+                    }
+                }
+            }, new Point(0, 80));
+
+
+
+            Content = absoluteLayout;
+        }
+    }
+}
