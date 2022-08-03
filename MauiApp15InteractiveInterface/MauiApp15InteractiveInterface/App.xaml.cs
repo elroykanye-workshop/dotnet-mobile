4e376ebb4264fa97f8598355a68ba26ad7a931ab
Elroy Kanye <elroykanye@gmail.com>
2022-08-03 05:03:55 +0100

    add first maui app

---
+++ MauiApp15InteractiveInterface/MauiApp15InteractiveInterface/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp15InteractiveInterface;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiApp15InteractiveInterface/MauiApp15InteractiveInterface/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp15InteractiveInterface;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
